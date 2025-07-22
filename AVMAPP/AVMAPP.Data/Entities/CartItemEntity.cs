74abb3c6db23610e56b118bcfd77865fa42a0f3c
Yusuf Çimen <yusufcimen90@windowslive.com>
2025-07-22 23:51:10 +0300

    Add CartItemEntity and ProductCommentEntity classes
    Implemented two new classes in the AVMAPP.Data.Entities namespace:
    CartItemEntity for managing cart item data and ProductCommentEntity
    for handling product comments. Both classes implement the
    IGenericField interface and include relevant properties for
    data management, enhancing the overall data model.

---
+++ AVMAPP/AVMAPP.Data/Entities/CartItemEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVMAPP.Data.Entities
+{
+    internal class CartItemEntity:IGenericField
+    {
+        public string UserId { get; set; }
+        public string ProductId { get; set; }
+        public byte Quantity { get; set; }
+
+        // IGenericField alanları
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Id { get; set; }
+    }
+}
