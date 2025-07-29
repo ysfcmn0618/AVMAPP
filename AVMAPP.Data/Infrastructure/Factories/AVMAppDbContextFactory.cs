e75f7daea725d15f2aa00da1b3b0b6a4523cc9c5
Yusuf Çimen <yusufcimen90@windowslive.com>
2025-07-30 00:42:32 +0300

    dosya yolu düzenlemesi

---
+++ AVMAPP.Data/Infrastructure/Factories/AVMAppDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using AVMAPP.Data.Infrastructure.AVMDbContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AVMAPP.Data.Infrastructure.Factories
+{
+    public class AVMAppDbContextFactory : IDesignTimeDbContextFactory<AVMAppDbContext>
+    {
+        AVMAppDbContext IDesignTimeDbContextFactory<AVMAppDbContext>.CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AVMAppDbContext>();
+
+            // ✅ Buraya kendi connection string'ini yaz
+            optionsBuilder.UseSqlServer("Server=YSFCMN-HOME\\SQLEXPRESS;Database=AVMAppSMadeDb;Trusted_Connection=True;TrustServerCertificate=Yes");
+            //var connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=AVMAppDb;Trusted_Connection=True;";
+            //optionsBuilder.UseSqlServer(connectionString);
+
+            return new AVMAppDbContext(optionsBuilder.Options);
+        }
+    }
+}
+
+
+
