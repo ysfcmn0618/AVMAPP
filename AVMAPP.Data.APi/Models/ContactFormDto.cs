4be065621bd95b8b1baa9619d56fec4ada3b4492
Yusuf Çimen <yusufcimen90@windowslive.com>
2025-08-18 14:51:12 +0300

    Add ContactFormController and DTO for contact forms
    Introduce ContactFormController to handle CRUD operations for contact forms.
    Implement ContactFormDto for data transfer with properties for form details.
    Add AutoMapper profile for mapping between DTO and entity.

---
+++ AVMAPP.Data.APi/Models/ContactFormDto.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using AVMAPP.Data.Entities;
+
+namespace AVMAPP.Data.APi.Models
+{
+    public class ContactFormDto
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public DateTime? SeenAt { get; set; }
+        public DateTime CreatedAt { get; set; }=DateTime.UtcNow;
+        public bool IsActive { get; set; }=true;
+        public bool IsDeleted { get; set; } = false;
+    }
+    public class ContactFormProfile:Profile
+    {
+        public ContactFormProfile()
+        {
+            CreateMap<ContactFormDto, ContactFormEntity>()
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true))
+                .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => false));
+            CreateMap<ContactFormEntity, ContactFormDto>();
+        }
+    }
+}
