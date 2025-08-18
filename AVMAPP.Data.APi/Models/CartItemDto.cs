92e2c7db13c418c7b617f32669316859ad9deb6d
Yusuf Çimen <yusufcimen90@windowslive.com>
2025-08-18 14:28:22 +0300

    Add CartItemController and DTO for cart item management
    Introduces CartItemController to handle HTTP requests for cart items, including methods for retrieving, adding, updating, and deleting cart items. Adds CartItemDto as a Data Transfer Object with relevant properties. Implements CartItemProfile for mapping between CartItemEntity and CartItemDto using AutoMapper.

---
+++ AVMAPP.Data.APi/Models/CartItemDto.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using AVMAPP.Data.Entities;
+
+namespace AVMAPP.Data.APi.Models
+{
+    public class CartItemDto
+    {       
+            public Guid UserId { get; set; }
+            public int ProductId { get; set; }
+            public byte Quantity { get; set; }          
+            public bool IsActive { get; set; }
+            public bool IsDeleted { get; set; }       
+    }
+    public class CartItemProfile:Profile
+    {
+        public CartItemProfile()
+        {
+            CreateMap<CartItemEntity, CartItemDto>()
+                .ReverseMap();
+        }
+    }
+}
