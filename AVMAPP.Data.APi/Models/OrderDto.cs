494b8ffd6107843fcb6935d26664f7397c4621f8
Yusuf Çimen <yusufcimen90@windowslive.com>
2025-08-19 12:06:02 +0300

    Add Order API controller and DTO implementation
    Created `OrderController.cs` for managing orders with methods to retrieve, create, update, and delete orders. Utilizes dependency injection and AutoMapper for object mapping.
    
    Added `OrderDto.cs` to define the data transfer object for orders, including properties for user, order code, and timestamps. Implemented `OrderProfile` for mapping between `OrderDto` and `OrderEntity`.

---
+++ AVMAPP.Data.APi/Models/OrderDto.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using AVMAPP.Data.Entities;
+
+namespace AVMAPP.Data.APi.Models
+{
+    public class OrderDto
+    {
+        public Guid UserId { get; set; }
+        public UserEntity User { get; set; }
+        public string OrderCode { get; set; }
+        public ICollection<OrderItemEntity>? OrderItems { get; set; }
+        // IGenericField alanları
+        public DateTime CreatedAt { get; set; }=DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<OrderDto, OrderEntity>().ReverseMap();
+        }
+
+
+    }
+}
+ 
