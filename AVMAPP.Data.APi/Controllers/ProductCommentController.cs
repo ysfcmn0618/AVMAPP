51b60a7fa7e5305db156b02afba917f518902476
Yusuf Çimen <yusufcimen90@windowslive.com>
2025-08-20 15:56:13 +0300

    Add ProductCommentController and DTO for comments
    Introduces a new `ProductCommentController` to manage HTTP
    requests for product comments, including methods for
    retrieving, updating, and deleting comments.
    
    Adds `ProductCommentDto` as a Data Transfer Object with
    necessary properties for comments.
    
    Implements `ProductCommentProfile` for AutoMapper
    configuration between `ProductCommentEntity` and
    `ProductCommentDto`.

---
+++ AVMAPP.Data.APi/Controllers/ProductCommentController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using AVMAPP.Data.APi.Models;
+using AVMAPP.Data.Entities;
+using AVMAPP.Data.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AVMAPP.Data.APi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCommentController(IGenericRepository<ProductCommentEntity> repo, IMapper mapper) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await repo.GetAllAsync();
+            var result = mapper.Map<IEnumerable<ProductCommentDto>>(comments);
+            return Ok(result);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var comment = await repo.GetByIdAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            var result = mapper.Map<ProductCommentDto>(comment);
+            return Ok(result);
+        }
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, [FromBody] ProductCommentDto comment)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var existingcomment = await repo.GetByIdAsync(id);
+            if (existingcomment == null)
+            {
+                return BadRequest("Comment bulunamadı");
+            }
+            comment.UpdatedAt = DateTime.UtcNow;
+            mapper.Map(comment, existingcomment);
+            await repo.Update(existingcomment);
+
+            return Ok(mapper.Map<ProductCommentDto>(existingcomment));
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var comment= await repo.GetByIdAsync(id);
+            if (comment == null) return BadRequest("Id eşleşmesi sağlanamadı.");
+            await repo.Delete(id);
+
+            return NoContent();
+        }
+
+    }
+}
