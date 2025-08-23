c1746795b970a76ee38b2f36089e0cd62ab48c94
Yusuf Çimen <yusufcimen90@windowslive.com>
2025-08-23 12:08:32 +0300

    Add ProductImage API controller and DTO
    Created `ProductImageController.cs` to manage product images with CRUD operations using a generic repository and AutoMapper. Added `ProductImageDto.cs` to define the data transfer object for product images, including properties and a mapping profile for entity conversion.

---
+++ AVMAPP.Data.APi/Controllers/ProductImageController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using AVMAPP.Data.APi.Models;
+using AVMAPP.Data.Entities;
+using AVMAPP.Data.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AVMAPP.Data.APi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImageController(IGenericRepository<ProductImageEntity> repo,IMapper mapper) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var entities = await repo.GetAllAsync();
+            var dtos = mapper.Map<IEnumerable<ProductImageDto>>(entities);
+            return Ok(dtos);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var entity = await repo.GetByIdAsync(id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            var dto = mapper.Map<ProductImageDto>(entity);
+            return Ok(dto);
+        }
+        [HttpGet("{prductId:int}")]
+        public async Task<IActionResult> GetByProductId(int prductId)
+        {
+            var entity = await repo.GetByIdAsync(prductId);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            var dto = mapper.Map<IEnumerable<ProductImageDto>>(entity);
+            return Ok(dto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductImageDto dto)
+        {
+            var entity = mapper.Map<ProductImageEntity>(dto);
+            var createdEntity = await repo.Add(entity);
+            var createdDto = mapper.Map<ProductImageDto>(createdEntity);
+            return CreatedAtAction(nameof(GetById), new { id = createdDto.Id }, createdDto);
+        }
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, ProductImageDto dto)
+        {
+            if (id != dto.Id)
+            {
+                return BadRequest();
+            }
+            var entity = mapper.Map<ProductImageEntity>(dto);
+            var updatedEntity = await repo.Update(entity);
+            var updatedDto = mapper.Map<ProductImageDto>(updatedEntity);
+            return Ok(updatedDto);
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deletedEntity = await repo.Delete(id);
+            if (deletedEntity == null)
+            {
+                return NotFound();
+            }
+            var deletedDto = mapper.Map<ProductImageDto>(deletedEntity);
+            return Ok(deletedDto);
+        }
+
+    }
+}
---
+++ AVMAPP.Data.APi/Models/ProductImageDto.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using AVMAPP.Data.Entities;
+
+namespace AVMAPP.Data.APi.Models
+{
+    public class ProductImageDto
+    {
+        public int ProductId { get; set; }
+        public ProductEntity? Product { get; set; }
+        public string Url { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public int Id { get; set; }
+    }
+    public class  ProductImageProfile:Profile
+    {
+        public ProductImageProfile()
+        {
+            CreateMap<ProductImageEntity, ProductImageDto>().ReverseMap();
+        }
+
+    }
+}
