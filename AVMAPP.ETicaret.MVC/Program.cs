91113028f683215cb4bc3660579b985b38a43019
Yusuf Çimen <yusufcimen90@windowslive.com>
2025-08-26 15:05:26 +0300

    automapper paket çakışmaları engellemek versiyon güncellemesi yapıldı

--- AVMAPP.ETicaret.MVC/Program.cs
+++ AVMAPP.ETicaret.MVC/Program.cs
@@ -1,17 +1,30 @@
 using AVMAPP.ETicaret.MVC.Handlers;
-
+using AVMAPP.Services;
+using AVMAPP.Services.Profiles;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddTransient<TokenAttachHandler>();
+builder.Services.ConfigureMvcServices(builder.Configuration);
 builder.Services.AddHttpClient("ApiClient", client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["ApiSettings:BaseUrl"]);
     client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 })
 .AddHttpMessageHandler<TokenAttachHandler>();
+
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ AVMAPP.Services/ServiceExtensionsMvc.cs
@@ -0,0 +1,26 @@
+﻿using AVMAPP.Services.Profiles;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVMAPP.Services
+{
+    public static class ServiceExtensionsMvc
+    {
+        public static void ConfigureMvcServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // API ile haberleşmek için HttpClient
+            services.AddHttpClient("ApiClient", client =>
+            {
+                client.BaseAddress = new Uri(configuration["ApiSettings:BaseUrl"]);
+            });
+
+            // AutoMapper (MVC tarafında ViewModel ↔ DTO eşlemeleri için)
+            services.AddAutoMapper(typeof(OrderItemProfile).Assembly);
+        }
+    }
+}
