081eeca38323d42f0a67e7c16964b4657a48f943
Yusuf Çimen <yusufcimen90@windowslive.com>
2025-08-26 07:55:48 +0300

    themanın style dosyaları eklendi genel basecontroller clasıı önceki rticaret uygulamasıdnan referans alınarak eklendi

---
+++ AVMAPP.ETicaret.MVC/Controllers/BaseController.cs
@@ -0,0 +1,65 @@
+﻿using AVMAPP.Models.DTo.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AVMAPP.ETicaret.MVC.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected T? GetService<T>() where T : class => HttpContext.RequestServices.GetService<T>();
+
+        protected void SetSuccessMessage(string message) => ViewBag.SuccessMessage = message;
+
+        protected void SetErrorMessage(string message) => ViewBag.ErrorMessage = message;
+
+        protected string? GetCookie(string key) => Request.Cookies[key];
+
+        protected void SetCookie(string key, string value) => Response.Cookies.Append(key, value);
+
+        protected void RemoveCookie(string key) => Response.Cookies.Delete(key);
+
+        protected async Task<UserDto?> GetCurrentUserAsync()
+        {
+
+            var userId = GetUserId();
+            if (userId is null) return null;
+
+            var clientFactory = GetService<IHttpClientFactory>();
+            if (clientFactory == null) return null;
+
+            var client = clientFactory.CreateClient("Api.Data");
+
+            var response = await client.GetAsync($"api/user/{userId}");
+            if (!response.IsSuccessStatusCode) return null;
+
+            var user = await response.Content.ReadFromJsonAsync<UserDto>();
+            return user;
+        }
+
+        protected bool IsUserLoggedIn() => /*GetCookie("userId") != null;*/User.Identity?.IsAuthenticated ?? false;
+
+        protected int? GetUserId() => /*int.TryParse(GetCookie("userId"), out int userId) ? userId : null;*/int.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out int userId) ? userId : null;
+
+
+        protected async Task<bool> IsUserAdminAsync()
+        {
+            var user = await GetCurrentUserAsync();
+
+            return user?.RoleId == RoleConstants.AdminRoleId;
+        }
+
+        protected async Task<bool> IsUserSellerAsync()
+        {
+            var user = await GetCurrentUserAsync();
+
+            return user?.RoleId == RoleConstants.SellerRoleId;
+        }
+
+        protected async Task<bool> IsUserBuyerAsync()
+        {
+            var user = await GetCurrentUserAsync();
+
+            return user?.RoleId == RoleConstants.BuyerRoleId;
+        }
+    }
+}
---
+++ AVMAPP.ETicaret.MVC/Controllers/RoleConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace AVMAPP.ETicaret.MVC.Controllers
+{
+    public static class RoleConstants
+    {
+        public static readonly Guid AdminRoleId = Guid.Parse("e1f49a71-5580-4d0f-b31f-a3552df64a92");
+        public static readonly Guid SellerRoleId = Guid.Parse("c5e8a91e-bcc9-4e0c-a602-b66e4217766e");
+        public static readonly Guid BuyerRoleId = Guid.Parse("d0ad2064-227a-4a60-b3ea-503b6cf3c407");
+    }
+}
