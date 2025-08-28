0ea9c30f7006d54e72ca52fa7c76571f0d5c529d
Yusuf Çimen <yusufcimen90@windowslive.com>
2025-08-28 10:16:34 +0300

    Add ImageUploadViewModel for managing image uploads
    Introduces the `ImageUploadViewModel` class in the
    `AVMAPP.Models.DTo.Dtos.Product` namespace. This class
    includes properties such as `IsRequired`, `IsMultipleFiles`,
    `Name`, `Label`, and `Accept` to facilitate image upload
    requirements. The `Name` property is non-nullable, while
    `Label` and `Accept` are nullable strings.

---
+++ AVMAPP.Moldels.DTo/Dtos/Product/ImageUploadViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVMAPP.Models.DTo.Dtos.Product
+{
+    public class ImageUploadViewModel
+    {
+        public bool IsRequired { get; set; }
+        public bool IsMultipleFiles { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Label { get; set; }
+        public string? Accept { get; set; }
+    }
+}
