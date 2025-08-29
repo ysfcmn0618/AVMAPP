610b6ad9225561c983104a511413988a4eb7a9bd
Yusuf Çimen <yusufcimen90@windowslive.com>
2025-08-29 19:51:45 +0300

    Admin panel için tema ve temanın partialllarının eklenmesi

--- AVMAPP.Admin.MVC/Program.cs
+++ AVMAPP.Admin.MVC/Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient("ApiClient", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5001/");
+});
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+    });
+
 
 var app = builder.Build();
 
@@ -18,6 +32,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
